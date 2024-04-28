d48e632184075e9e4d360a7e0854716beaa237b5
Aryan Sapra <aryansaprajiit@gmail.com>
2024-04-28 22:14:25 +0530

    servlet setup

---
+++ src/main/java/com/servlet/FirstServlet.java
@@ -0,0 +1,38 @@
+package com.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet implements Servlet{
+    private ServletConfig config;
+    @Override
+    public void init(ServletConfig config) {
+        this.config = config;
+        System.out.println("Initializations");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.out.println("Servicing........");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>I am writing from service class</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroying........");
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2>My First Servlet!</h2>
+</body>
+</html>

afca75458773734bdfa33e47dde0a6c05e17a905
Aryan Sapra <aryansaprajiit@gmail.com>
2024-04-28 22:29:06 +0530

    servlet created using GenericServlet class

---
+++ src/main/java/com/servlet/SecondServlet.java
@@ -0,0 +1,17 @@
+package com.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SecondServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("<h1>created using GenericServlet</h1>");
+    }
+}
